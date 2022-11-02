48f79cc369ea29abea6b8617711ec019c1ac47d6
Hai Nguyen <54726078+hai-nguyen93@users.noreply.github.com>
2022-11-01 19:16:15 -0700

    add push back wave vfx

--- Assets/Scripts_Survivor/Player/Surv_YameroWave.cs
+++ Assets/Scripts_Survivor/Player/Surv_YameroWave.cs
@@ -10,12 +10,23 @@ public class Surv_YameroWave : Surv_PlayerAttack
     public LayerMask enemyLayer;
     public float attackradius { get => radiusAtLevel[Mathf.Min(level, ATTACK_MAX_LV) - 1]; }
     public float pushBackForce { get => pushBackForceAtLevel[Mathf.Min(level, ATTACK_MAX_LV) - 1]; }
+
+    [Header("Visual Settings")]
     public GameObject telegraph;
+    public Transform visualEffect;
+    public Vector2 waveLerpRange;
+    private Renderer vfxRenderer;
+    private MaterialPropertyBlock vfxMatPropBlock;
+    public float visualDuration = 0.5f;
 
     protected override void Start()
     {
         base.Start();
         telegraph.SetActive(false);
+
+        visualEffect.gameObject.SetActive(false);
+        vfxMatPropBlock = new MaterialPropertyBlock();
+        vfxRenderer = visualEffect.GetComponent<Renderer>();
     }
 
     private void Update()
@@ -25,7 +36,7 @@ public class Surv_YameroWave : Surv_PlayerAttack
 
     public override void Attack()
     {
-        StartCoroutine(ShowTelegraph());
+        StartCoroutine(ShowVFX());
 
         var hits = Physics.OverlapSphere(transform.position, attackradius, enemyLayer);
         foreach (var hit in hits)
@@ -40,12 +51,27 @@ public class Surv_YameroWave : Surv_PlayerAttack
         ResetAttackTimer();
     }
 
-    private IEnumerator ShowTelegraph()
+    private IEnumerator ShowVFX()
     {
         telegraph.SetActive(true);
         telegraph.transform.localScale = new Vector3(2 * attackradius, 2 * attackradius, 1f);
-        yield return new WaitForSeconds(0.5f);
+
+        visualEffect.gameObject.SetActive(true);
+        visualEffect.localScale = new Vector3(2 * attackradius, 2 * attackradius, 1f);
+
+        float t = visualDuration;
+        while (t > 0f) {
+            vfxRenderer.GetPropertyBlock(vfxMatPropBlock);
+            float amount = Mathf.Lerp(waveLerpRange.x, waveLerpRange.y, 1 - (t / visualDuration));
+            vfxMatPropBlock.SetFloat("_LerpAmount", amount);
+            vfxRenderer.SetPropertyBlock(vfxMatPropBlock);
+
+            t -= Time.deltaTime;
+            yield return null;
+        }
+
         telegraph.SetActive(false);
+        visualEffect.gameObject.SetActive(false);
     }
 
     public override void OnValidate()
